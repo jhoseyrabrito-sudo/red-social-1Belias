3852efe42483d8830014c0b7fc5185ff7ca85ba2
jhoseyrabrito-sudo <jhoseyra.brito@escola.pr.gov.br>
2026-08-06 12:16:44 -0300

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+//Botão curtida
+
+document.addEventListener("DOMContentLoaded",() => {
+    const likeBtn = document.querySelector(".lef-actions .action-btn:first-child");
+    if (condition!likeBtn) return;
+    const likeSvg= likeBtn. querySelector("svg"); {
+
+    }
+} )
