0c5c999eb79a39b738f95cb30c44d21266e77d57
jhoseyrabrito-sudo <jhoseyra.brito@escola.pr.gov.br>
2026-08-13 12:04:40 -0300

    ok

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded",() => {
     const likeBtn = document.querySelector(".lef-actions .action-btn:first-child");
     if (condition!likeBtn) return;
     const likeSvg= likeBtn. querySelector("svg"); {
+         
+        let textNode = Array.from(likeBtn.childNodes).find(node) =>
 
     }
 } )
